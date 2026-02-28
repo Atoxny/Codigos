36d8ccf8a467182977c2f2549156598aafaff497
Anthonny <choquealex122@gmail.com>
2026-02-27 21:54:52 -0400

    [Segundo Commit]

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,7 @@
 public class Prueba {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo!");
+        //System.out.println("Hola Mundo!");
+        Persona p = new Persona("Alex");
+        p.leer();
     }
 }
-
